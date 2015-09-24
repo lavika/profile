1b1fba4a6b40b9ed4bab7713a5cddc3a4f1e8d2f
Lavika <k_lavika16@yahoo.com>
2015-09-24 16:21:37 +0530

    few things changed

--- db/schema.rb
+++ db/schema.rb
@@ -13,30 +13,15 @@
 
 ActiveRecord::Schema.define(version: 20150923044645) do
 
-  create_table "articles", force: :cascade do |t|
-    t.string   "title",      limit: 255
-    t.text     "text",       limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.string   "commenter",  limit: 255
-    t.text     "body",       limit: 65535
-    t.integer  "article_id", limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-  end
-
-  add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "messages", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "email",      limit: 255
-    t.text     "message",    limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "name"
+    t.string   "email"
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "articles"
 end
